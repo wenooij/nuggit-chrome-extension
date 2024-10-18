33b0d52e737fd70da2d15f95114b470c2abe4a9d
wenooij <142677213+wenooij@users.noreply.github.com>
2024-10-18 02:52:03 -0400

    Update extension

--- background.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Data Extractor Extension Installed");
-});
--- content.js
+++ content.js
@@ -1,6 +1,42 @@
-// content.js
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getHTML") {
-      sendResponse({ html: document.documentElement.outerHTML });
+console.log("Nuggit was injected into this page and may be collecting data (https://github.com/wenooij/nuggit-chrome-extension).");
+
+const host = 'http://localhost:9402';
+
+async function triggerRequest() {
+  try {
+    const response = await fetch(host + '/api/trigger', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'trigger': {
+          'implicit': true,
+          'url': document.location.href,
+          'timestamp': new Date(),
+        },
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  });
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+}
+
+async function trigger() {
+  endpoint = host + '/api/trigger';
+  url = document.location.href;
+
+  const response = await triggerRequest();
+
+  console.log(response);
+}
+
+trigger();
